916e6af0eaf65059addc6c334ac28877954e5f98
guptadhruvnarayan10-cmyk <guptadhruvnarayan10@gmail.com>
2026-06-13 11:06:09 +0000

    avg of 2 numbers

---
+++ basic.cpp
@@ -0,0 +1,11 @@
+// program for finding the avg of 2 nnumbers
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter a and b";
+    cin>>a>>b;
+    cout<<(a+b)/2<<endl;
+    return 0;
+}
