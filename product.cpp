f5831e4237e9bc1e95dd6c66f82a0a6d801c41ce
guptadhruvnarayan10-cmyk <guptadhruvnarayan10@gmail.com>
2026-06-13 11:08:15 +0000

    product of 3 numbers

---
+++ product.cpp
@@ -0,0 +1,11 @@
+// program for product of 3 numbers
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter a,b,c"<<endl;
+    cin>>a>>b>>c;
+    cout<<a*b*c<<endl;
+    return 0;
+}
